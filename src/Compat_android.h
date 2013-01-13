ac239e71302ec143f501e1abd5962710c9c42952
Mikkel Krautz <mikkel@krautz.dk>
2013-01-13 02:18:05 +0100

    Compat_android: fix header guards and copyright year.

--- src/Compat_android.h
+++ src/Compat_android.h
@@ -2,6 +2,11 @@
 // The use of this source code is goverened by a BSD-style
 // license that can be found in the LICENSE-file.
 
+#ifndef MUMBLE_COMPAT_ANDROID_H_
+#define MUMBLE_COMPAT_ANDROID_H_
+
 #include <ctime>
 
 std::time_t timegm(struct tm *tm);
+
+#endif
